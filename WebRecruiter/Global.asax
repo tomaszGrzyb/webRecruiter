78adfb26e18420ae42d368f621987e4bb4a4546a
Tomasz Grzyb <t0mush@outlook.com>
2018-12-16 16:32:03 +0100

    Add project files.

---
+++ WebRecruiter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebRecruiter.MvcApplication" Language="C#" %>
---
+++ WebRecruiter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebRecruiter.Startup))]
+namespace WebRecruiter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
