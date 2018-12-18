8b9f95f3b085fa57692973918d14478161eef516
Tomasz Grzyb <t0mush@outlook.com>
2018-12-18 23:30:14 +0100

    Added ViewModels folder and moved there files from Model

--- WebRecruiter/ViewModels/ManageViewModels.cs
+++ WebRecruiter/ViewModels/ManageViewModels.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
-namespace WebRecruiter.Models
+namespace WebRecruiter.ViewModels
 {
     public class IndexViewModel
     {
