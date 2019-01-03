e96961e3acde97e1f47a087ab85ad1af86a7dca6
Tomasz Grzyb <tomek.g@o2.pl>
2019-01-04 00:30:10 +0100

    updated Candidate annotation

--- WebRecruiter/Models/Candidate.cs
+++ WebRecruiter/Models/Candidate.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,17 +10,36 @@ namespace WebRecruiter.Models
 	public class Candidate
 	{
 		public int Id { get; set; }
+
+		[DisplayName("Imię")]
 		public string FirstName { get; set; }
+
+		[DisplayName("Drugie Imię")]
 		public string SecondName { get; set; }
+
+		[DisplayName("Nazwisko")]
 		public string LastName { get; set; }
+
+		[DisplayName("Data urodzenia")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
 		public DateTime DateOfBirth { get; set; }
+
+		[DisplayName("Miejsce urodzenia")]
 		public string PlaceOfBirth { get; set; }
+
+		[DisplayName("Numer telefonu")]
 		public string PhoneNumber { get; set; }
+
+		[DisplayName("Pesel")]
 		public string Pesel { get; set; }
+
 		public int AddressId { get; set; }
 		public Address Address { get; set; }
+
 		public int DocumentId { get; set; }
 		public Document Document { get; set; }
+
 	    public string UserId { get; set; }
 	    public ApplicationUser User { get; set; }
 
