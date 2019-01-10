a6534e51e813dbaeeac04c56ebcbc535b9909107
Tomasz Grzyb <tomek.g@o2.pl>
2019-01-10 16:16:55 +0100

    navbar

--- WebRecruiter/App_Start/BundleConfig.cs
+++ WebRecruiter/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace WebRecruiter
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+					  "~/Content/Site.css"));
 		}
 	}
 }
